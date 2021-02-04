2c937d2d8d09e02a17e1c44841a12aa8ea66a890
Jim Li <jli263@uic.edu>
2021-02-04 13:18:29 -0600

    Changed ball to star

--- sketch.js
+++ sketch.js
@@ -1,3 +1,17 @@
+/* Formal elements:
+ * The major formal elements used are shapes, noise, movement. 
+ * The star shape moves on its own and will change directions on wall collisions as well as on user input.
+ * Furthermore, colors will change randomly depending on the user input. 
+ * 
+ * How to interact:
+ * Left and right arrow to change directions of 
+ * Up arrow key to change to color to a random darker color
+ * Down arrow key to change the color to a random lighter color
+ * Space to change to color to black
+ * 
+ */ 
+
+
 class myEllipse {
 
     constructor(x, y, w, h) {
@@ -19,13 +33,32 @@ let _ellipse = {
     YSpeed : 5
 };
 
-let ellipseArray = [];
+let _triangle1 = {
+    x1 : 20,
+    x2 : 45,
+    x3 : 75,
+    y1 : 65,
+    y2 : 20,
+    y3 : 65
+};
+
+let _triangle2 = {
+    x1 : 20,
+    x2 : 45,
+    x3 : 75,
+    y1 : 30,
+    y2 : 75,
+    y3 : 30
+};
+
 
 let mappedMouseY = 0;
 
 let cnv;
 
 document.onkeydown = checkKey;
+
+let _noise = 0.0;
 
 //Setup the canvas
 function setup() {
@@ -45,21 +78,53 @@ function draw() {
 
     mappedMouseY = map(mouseY, 0, windowHeight, 0, 128);
 
-    //background("#ffffff01");
 
-    drawEllipse();
+    _noise = _noise + 0.01;
+
+
+    drawStar();
 }
 
 //Draw the ellipse 
-function drawEllipse() {
-
-    let e = new myEllipse(_ellipse.X,_ellipse.Y,50,50);
-    e.show();
+function drawStar() {
     
+    triangle(_triangle1.x1,_triangle1.y1,_triangle1.x2,_triangle1.y2,_triangle1.x3,_triangle1.y3 );
+    triangle(_triangle2.x1,_triangle2.y1,_triangle2.x2,_triangle2.y2,_triangle2.x3,_triangle2.y3 );
+
+    
+    if ((_triangle1.x3 >= windowWidth && _ellipse.XSpeed >= 0) || (_triangle1.x1 <= 0 && _ellipse.XSpeed < 0)) {
+        if (_ellipse.XSpeed < 0)
+            _ellipse.XSpeed = -1 * (noise(_noise) * 6 + 5)
+        else
+            _ellipse.XSpeed = (noise(_noise) * 6)  + 5
+
+        _ellipse.XSpeed*=-1;
+    }
+    //Bounce between the max and min width
+    if ((_triangle2.y2 >= windowHeight &&_ellipse.YSpeed >= 0) || (_triangle1.y1 <= 0 && _ellipse.YSpeed < 0)) {
+        if (_ellipse.YSpeed < 0)
+            _ellipse.YSpeed = -1 * (noise(_noise) * 6 + 5)
+        else
+            _ellipse.YSpeed = (noise(_noise) * 6)  + 5
+        
+        _ellipse.YSpeed*=-1;
+    }
+
+    _triangle1.x1+=_ellipse.XSpeed;
+    _triangle1.y1+=_ellipse.YSpeed;
+    _triangle1.x2+=_ellipse.XSpeed;
+    _triangle1.y2+=_ellipse.YSpeed;
+    _triangle1.x3+=_ellipse.XSpeed;
+    _triangle1.y3+=_ellipse.YSpeed;
+    _triangle2.x1+=_ellipse.XSpeed;
+    _triangle2.y1+=_ellipse.YSpeed;
+    _triangle2.x2+=_ellipse.XSpeed;
+    _triangle2.y2+=_ellipse.YSpeed;
+    _triangle2.x3+=_ellipse.XSpeed;
+    _triangle2.y3+=_ellipse.YSpeed;
+
     /*  NOT USED
     ellipseArray.push(e);
-
-    console.log(ellipseArray.length);
 
     for (let i = 0; i < ellipseArray.length; i++) {
         ellipseArray[i].show();
@@ -69,6 +134,8 @@ function drawEllipse() {
         ellipseArray.splice(0,2);
     } */
  
+
+    /*
     //Bounce between the max and min width
     if (_ellipse.X > windowWidth-30 || _ellipse.X < 30) {
         _ellipse.XSpeed*=-1;
@@ -80,7 +147,7 @@ function drawEllipse() {
 
     //Move the ellipse
     _ellipse.X = _ellipse.X + _ellipse.XSpeed;
-    _ellipse.Y = _ellipse.Y + _ellipse.YSpeed;
+    _ellipse.Y = _ellipse.Y + _ellipse.YSpeed; */
 }
 
 function changeColor(type) {
@@ -95,7 +162,7 @@ function changeColor(type) {
     fill(Math.floor(Math.random() * 127) + init, Math.floor(Math.random() * 127) + init, Math.floor(Math.random() * 127) + init);
 
     //Set the stroke
-    stroke(Math.floor(Math.random() * 127) + init, Math.floor(Math.random() * 127) + init, Math.floor(Math.random() * 127) + init, 50);
+    stroke(Math.floor(Math.random() * 127) + init, Math.floor(Math.random() * 127) + init, Math.floor(Math.random() * 127) + init);
 }
 
 //Change fill and stroke color -- NOT USED
