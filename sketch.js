16744692130b8dddf10aa88cd95b41af024c368e
Jim Li <jli263@uic.edu>
2021-02-08 16:51:59 -0600

    Added the ability to change sizes

--- sketch.js
+++ sketch.js
@@ -8,30 +8,13 @@
  * Up arrow key to change to color to a random darker color
  * Down arrow key to change the color to a random lighter color
  * Space to change to color to black
+ * Z key to decrease the size
+ * X key to increase the size
  * 
  */ 
-
-
-class myEllipse {
-
-    constructor(x, y, w, h) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-    }
-
-    show() {
-        ellipse(this.x, this.y, this.w, this.h);        //Draw the ellipse
-    }
-}
-
-let _ellipse = {
-    X : 30,
-    Y : 30,
-    XSpeed : 5,
-    YSpeed : 5
-};
+ 
+let XSpeed = 5;
+let YSpeed = 5;
 
 let _triangle1 = {
     x1 : 20,
@@ -51,14 +34,13 @@ let _triangle2 = {
     y3 : 30
 };
 
-
-let mappedMouseY = 0;
-
 let cnv;
 
 document.onkeydown = checkKey;
 
 let _noise = 0.0;
+
+let sizeLimit = 0;
 
 //Setup the canvas
 function setup() {
@@ -69,85 +51,56 @@ function setup() {
     //Set the initial colors
     changeColor(false);
 
-    strokeWeight(2);    //Set stroke weight
+    strokeWeight(1);    //Set stroke weight
 
     background(0,0,0); //Set background
 }
 
 function draw() {
-
-    mappedMouseY = map(mouseY, 0, windowHeight, 0, 128);
-
-
     _noise = _noise + 0.01;
-
-
     drawStar();
 }
 
-//Draw the ellipse 
+
 function drawStar() {
     
+    //Draw the star
     triangle(_triangle1.x1,_triangle1.y1,_triangle1.x2,_triangle1.y2,_triangle1.x3,_triangle1.y3 );
     triangle(_triangle2.x1,_triangle2.y1,_triangle2.x2,_triangle2.y2,_triangle2.x3,_triangle2.y3 );
 
-    
-    if ((_triangle1.x3 >= windowWidth && _ellipse.XSpeed >= 0) || (_triangle1.x1 <= 0 && _ellipse.XSpeed < 0)) {
-        if (_ellipse.XSpeed < 0)
-            _ellipse.XSpeed = -1 * (noise(_noise) * 6 + 5)
+    //Bounce between max and min width
+    if ((_triangle1.x3 >= windowWidth && XSpeed >= 0) || (_triangle1.x1 <= 0 && XSpeed < 0)) {
+        if (XSpeed < 0)
+            XSpeed = -1 * (noise(_noise) * 6 + 5)
         else
-            _ellipse.XSpeed = (noise(_noise) * 6)  + 5
+            XSpeed = (noise(_noise) * 6)  + 5
 
-        _ellipse.XSpeed*=-1;
+        XSpeed*=-1;
     }
-    //Bounce between the max and min width
-    if ((_triangle2.y2 >= windowHeight &&_ellipse.YSpeed >= 0) || (_triangle1.y1 <= 0 && _ellipse.YSpeed < 0)) {
-        if (_ellipse.YSpeed < 0)
-            _ellipse.YSpeed = -1 * (noise(_noise) * 6 + 5)
+
+    //Bounce between max and min height
+    if ((_triangle2.y2 >= windowHeight && YSpeed >= 0) || (_triangle1.y2 <= 0 && YSpeed < 0)) {
+        if (YSpeed < 0)
+            YSpeed = -1 * (noise(_noise) * 6 + 5)
         else
-            _ellipse.YSpeed = (noise(_noise) * 6)  + 5
+            YSpeed = (noise(_noise) * 6)  + 5
         
-        _ellipse.YSpeed*=-1;
+        YSpeed*=-1;
     }
 
-    _triangle1.x1+=_ellipse.XSpeed;
-    _triangle1.y1+=_ellipse.YSpeed;
-    _triangle1.x2+=_ellipse.XSpeed;
-    _triangle1.y2+=_ellipse.YSpeed;
-    _triangle1.x3+=_ellipse.XSpeed;
-    _triangle1.y3+=_ellipse.YSpeed;
-    _triangle2.x1+=_ellipse.XSpeed;
-    _triangle2.y1+=_ellipse.YSpeed;
-    _triangle2.x2+=_ellipse.XSpeed;
-    _triangle2.y2+=_ellipse.YSpeed;
-    _triangle2.x3+=_ellipse.XSpeed;
-    _triangle2.y3+=_ellipse.YSpeed;
-
-    /*  NOT USED
-    ellipseArray.push(e);
-
-    for (let i = 0; i < ellipseArray.length; i++) {
-        ellipseArray[i].show();
-    }
-
-    if (ellipseArray.length > 2000) {
-        ellipseArray.splice(0,2);
-    } */
- 
-
-    /*
-    //Bounce between the max and min width
-    if (_ellipse.X > windowWidth-30 || _ellipse.X < 30) {
-        _ellipse.XSpeed*=-1;
-    }
-    //Bounce between the max and min width
-    if (_ellipse.Y > windowHeight-20 || _ellipse.Y < 20) {
-        _ellipse.YSpeed*=-1;
-    }
-
-    //Move the ellipse
-    _ellipse.X = _ellipse.X + _ellipse.XSpeed;
-    _ellipse.Y = _ellipse.Y + _ellipse.YSpeed; */
+    //Change the location of the star
+    _triangle1.x1+=XSpeed;
+    _triangle1.y1+=YSpeed;
+    _triangle1.x2+=XSpeed;
+    _triangle1.y2+=YSpeed;
+    _triangle1.x3+=XSpeed;
+    _triangle1.y3+=YSpeed;
+    _triangle2.x1+=XSpeed;
+    _triangle2.y1+=YSpeed;
+    _triangle2.x2+=XSpeed;
+    _triangle2.y2+=YSpeed;
+    _triangle2.x3+=XSpeed;
+    _triangle2.y3+=YSpeed;
 }
 
 function changeColor(type) {
@@ -165,39 +118,18 @@ function changeColor(type) {
     stroke(Math.floor(Math.random() * 127) + init, Math.floor(Math.random() * 127) + init, Math.floor(Math.random() * 127) + init);
 }
 
-//Change fill and stroke color -- NOT USED
-function changeColorAndRot() {
-   
-    changeColor();
-
-    let randNum = Math.floor(Math.random() * 4);
-
-    if (randNum == 0){
-        _ellipse.YSpeed*=-1;
-        _ellipse.XSpeed*=-1;
-    }
-    else if (randNum == 1 || randNum == 3) {
-        _ellipse.YSpeed*=1;
-        _ellipse.XSpeed*=-1;
-    }
-    else if (randNum == 2 || randNum == 4) {
-        _ellipse.YSpeed*=-1;
-        _ellipse.XSpeed*=1;
-    }
-}
-
 function checkKey(e) {
 
     e = e || window.event;
     
     //Check for the keyCode
     if (e.keyCode == 37) {
-        _ellipse.YSpeed*=-1;
-        _ellipse.XSpeed*=1;
+        YSpeed*=-1;
+        XSpeed*=1;
     }
     else if (e.keyCode == 39) {
-        _ellipse.YSpeed*=1;
-        _ellipse.XSpeed*=-1;
+        YSpeed*=1;
+        XSpeed*=-1;
     }
     else if(e.keyCode == 32) {
         //Set color to black
@@ -210,4 +142,36 @@ function checkKey(e) {
    else if (e.keyCode == 40) {
         changeColor(true);
    }
+   else if (e.keyCode == 88) {
+       if(sizeLimit > -2) {
+            sizeLimit--;
+
+            _triangle1.x1-=15; 
+            _triangle1.y1+=10;
+            _triangle1.y2-=15; 
+            _triangle1.x3+=15; 
+            _triangle1.y3+=10;
+            _triangle2.x1-=15; 
+            _triangle2.y1-=10;
+            _triangle2.y2+=15; 
+            _triangle2.x3+=15; 
+            _triangle2.y3-=10;
+       }
+   }
+   else if (e.keyCode == 90) {
+       if (sizeLimit < 2) {
+            sizeLimit++;
+
+            _triangle1.x1+=15; 
+            _triangle1.y1-=10;
+            _triangle1.y2+=15; 
+            _triangle1.x3-=15; 
+            _triangle1.y3-=10;
+            _triangle2.x1+=15; 
+            _triangle2.y1+=10;
+            _triangle2.y2-=15; 
+            _triangle2.x3-=15; 
+            _triangle2.y3+=10;
+       }
+    }
 }
